097bc026a6e63d5d61ea199db99b05ae8e99b97d
gangming <ming.gang@immetalk.com>
2017-09-06 14:41:01 +0800

    no message

--- mysite/views.py
+++ mysite/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.template.response import TemplateResponse
 import datetime
 def hello(request):
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%I:%S")
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     name = {'name': 'zhang'}
+    # request =' %s', % (request)
+    host = request.get_host()
     return TemplateResponse(request, 'hello.html', locals())
