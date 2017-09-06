183c2446cb6173ee6004afeadf0d9d76d381b175
gangming <ming.gang@immetalk.com>
2017-09-06 15:08:40 +0800

    no message

---
+++ mysite/models.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# __*__ encoding: utf-8 __*__
+from django.db import models
+
+class User(models.Model):
+    """docstring for user"""
+    # id = models.CharField(max_length=32)
+    user_id = models.CharField(max_length=12)
+    user_name = models.CharField(max_length=10)
+    email = models.CharField(max_length=20)
+    cellphone = models.CharField(max_length=15)
+
+    def _str__(self):
+        return self.name
