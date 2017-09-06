1d592ee626d14f05eece79fac8e5057478f23fa7
gangming <ming.gang@immetalk.com>
2017-09-06 16:00:01 +0800

    no message

--- README.md
+++ README.md
@@ -2,8 +2,23 @@
 - 初始化 开启服务
 
         python manage.py migrate
-	python manage.py runserver
+        python manage.py runserver
+	
 
 	默认http://127.0.0.1:8000
+   
+
 - 安装mysqlclient
-    pip install mysqlclient
+
+        pip install mysqlclient
+
+- model
+
+   
+	1. 创建models.py文件 定义model 
+	2. 在settings.py 中 INSTALLED_APPS 添加 'mysite' 应用
+	3. python manage.py makemigrations mysite
+	4. python manage.py migrate
+    
+    
+    
--- mysite/views.py
+++ mysite/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
 import datetime
+from mysite.models import User
+
 def hello(request):
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     name = {'name': 'zhang'}
+    user = User()
+    user.user_name = 'username'
+    user.user_id = 'user_id'
+    user.email = 'email@email.com'
+    user.cellphone = '123456789098'
+    user.save()
     # request =' %s', % (request)
     host = request.get_host()
     return TemplateResponse(request, 'hello.html', locals())
